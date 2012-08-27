e2939e458ce593d948495b0eeda850934af26af3
quentinsence <quentin@counterinception.com>
2012-08-27 11:01:40 +0100

    merged home.r and office.r

---
+++ homeoffice.r
@@ -0,0 +1,164 @@
+#TODO
+#adjust times between office and home
+#add KCL pollution feeds
+#only display radiation time series when event detected
+
+#add ablines for quartiles
+#rescale dust and pollutants
+#fix local clock and timezone settings
+    
+library(maptools)
+library(maps)
+library(RCurl)
+data(world.cities)
+
+#local config
+source("local.r")
+
+##########
+s <- read.csv("logs.csv",header=FALSE)
+#g <- read.csv("co2.csv",header=FALSE)
+d <- read.csv("dust.csv",header=FALSE)
+g <- read.csv("cpm.geiger.csv",header=FALSE)
+
+names(s) <- c('V2','V3','V4','V5','V6','V7','V8','V9','V10')
+#s$V1 <- NULL
+
+s$t <- as.POSIXct(s$V2,origin="1970-01-01",tz=tz)
+d$t <- as.POSIXct(d$V1,origin="1970-01-01",tz=tz)
+g$t <- as.POSIXct(g$V1+7200,origin="1970-01-01",tz=tz)
+
+s$dt <- c(0,diff(s$V2))
+
+#adjust for arduino clock inaccuracies, both feeds were picked at the same time so last timestamp should be identical in both feeds
+#g$t <- g$t + s$V2[length(s$V2)] - g$V1[length(g$V1)]
+d$t <- d$t + s$V2[length(s$V2)] - d$V1[length(d$V1)]
+
+#s$V2 <- s$V2 - 3600;
+t0 <- s$V2[1]
+t1 <- s$V2[length(s$V2)] - 86400 * 0
+
+#show only last 24 hours
+t0 <- t1 - 86400
+#t0 <- t1 - 60000;
+#get last and next alarm time
+talarm1 <- t1 + s$V7[length(s$V7)]
+#24h before to get previous alarm time, assuming the alarm time has not been changed since
+talarm0 <- talarm1 - 86400
+
+cat('home last entry:',strftime(as.POSIXct(t1,origin="1970-01-01"),format="%Y-%m-%d %A"),'\n')
+cat('radiation max:',max(g$V2),'cpm on',strftime(as.POSIXct(g$V1[which(g$V2 == max(g$V2))],origin="1970-01-01"),format="%Y-%m-%d %A %X"),'\n')
+cat('mean radiation last 24h:',mean(g$V2[g$t > t0]),'cpm\n')
+
+w <- na.omit(read.csv("wth.txt"))
+
+#stored as UTC, switch to BST
+w$time <- w$time + 3600
+#drop ip, only 1 user/collection point for now
+w$ip <- NULL
+
+#dump everything in a 24h period from 0 to 86400 seconds for hourly/time of day stats
+w$time24 <- w$time %%86400
+
+w$time <- as.POSIXct(w$time,origin="1970-01-01")
+w$weekday <- format.POSIXct(w$time,format="%w")
+sweekday <- c('Sun','Mon','Tue','Wed','Thu','Fri','Sat')
+
+#remove tea cups before 7:30am = artefacts, morning cleaners are disrupting sensors
+w$tea[w$time24 < 7.5*3600] <- 0
+
+#boundaries to display only the last 24 hours
+t0 <- w$time[1]
+t1 <- w$time[length(w$time)]
+t0 <- t1 - 86400 * 1
+#t0 <- as.POSIXct(paste(substr(t1,1,10),"00:00:00"))
+#t1 <- t0 + 86400
+
+#t0 <- t0 - 86400
+#t1 <- t1 - 86500
+
+circadian <- function(t0,t1) {
+  
+  for (t in seq(t0,t1,86400)) {
+    t <- as.POSIXct(t,origin="1970-01-01")
+    #working day start and end times
+    work0 <- as.POSIXct(paste(substr(t,1,10),"09:00:00"))
+    work1 <- as.POSIXct(paste(substr(t,1,10),"17:30:00"))
+    #sunrise sunset times
+    sunrise <- sunriset(matrix(c(long,lat), nrow = 1),t,direction = "sunrise",POSIXct.out = TRUE)
+    sunset  <- sunriset(matrix(c(long,lat), nrow = 1),t,direction = "sunset" ,POSIXct.out = TRUE)
+    abline(v=work0,col="red")
+    abline(v=work1,col="red")
+    abline(v=sunrise,col="orange")
+    abline(v=sunset,col="orange")
+  }
+}
+
+#calculate daily maximum temperatures
+w$day <- as.Date(w$time)
+wmaxt <- tapply(w$temperature,w$day,max)
+# max(wmaxt)
+#percentage of days colder than today
+hotter <- 100 * (1 - ((length(wmaxt[wmaxt >= wmaxt[length(wmaxt)]]) - 1) / dim(wmaxt)))
+cat('office today is hotter than',hotter,'% of the days since office move',dim(wmaxt),'days ago\n')
+
+cat('office hottest day ever:',names(wmaxt[which(wmaxt == max(wmaxt),arr.ind=TRUE)]),'at',wmaxt[which(wmaxt == max(wmaxt))],'C')
+
+#should use subset to count only teas after 7am
+td <- aggregate(w$tea[w$time24 > 7*3600],list(w$day[w$time24 > 7*3600]),sum)
+names(td) <- c('day','tea')
+td$weekday <- strftime(td$day,format="%w")
+##EXTRA PLOTS
+#boxplot(td$tea ~ td$weekday,names=sweekday,ylab="number of teas (250ml)",main="number of tea cups by day of the week")
+#tea time distribution in 15 min chunks
+#hist(w$time24[w$tea > 0]/3600,breaks=24*4)
+
+#hist(wmaxt)
+#abline(v=wmaxt[length(wmaxt)])
+
+
+par(mfrow=c(10,1),mai=c(0,0.8,0,0),lab=c(10,10,7));
+
+plot(s$V4~s$t,type="l",xlim=c(t0,t1),ylim=c(min(s$V4[s$t>t0]),max(s$V4[s$t>t0])),ylab="temp (C)",xaxt="n");
+axis.POSIXct(1, at=seq(as.POSIXct(t0,origin="1970-01-01",tz=tz),as.POSIXct(t1,origin="1970-01-01",tz=tz),by="hour"),format="%H:%M")
+plot(s$V5~s$t,type="l",xlim=c(t0,t1),ylim=c(min(s$V5[s$t>t0]),max(s$V5[s$t>t0])),ylab="humidity (%Rh)",xaxt="n");
+#missed timestamps
+axis.POSIXct(1, at=seq(as.POSIXct(t0,origin="1970-01-01",tz=tz),as.POSIXct(t1,origin="1970-01-01",tz=tz),3600),format="%H:%M")
+plot(s$V6~s$t,type="l",xlim=c(t0,t1),ylab="light",xaxt="n");
+rug(s$V2[s$V3 == 0 & s$V2 > t0],ticksize=0.06,col="blue")
+#add sunrise and sunset
+sunrise <- sunriset(matrix(c(long,lat), nrow = 1),as.POSIXct(t1,origin="1970-01-01",tz=tz),direction = "sunrise",POSIXct.out = TRUE)
+sunset <- sunriset(matrix(c(long,lat), nrow = 1),as.POSIXct(t0,origin="1970-01-01",tz=tz),direction = "sunset",POSIXct.out = TRUE)
+abline(v=sunrise$time,col="orange")
+abline(v=sunset$time,col="orange")
+abline(v=talarm0,col="red")
+abline(v=talarm1,col="red")
+axis.POSIXct(1, at=seq(as.POSIXct(t0,origin="1970-01-01",tz=tz),as.POSIXct(t1,origin="1970-01-01",tz=tz),3600),format="%H:%M")
+plot(s$V9~s$t,type="l",xlim=c(t0,t1),ylab="pollutants",ylim=c(min(d$V2[d$t > t0]),max(d$V2[d$t > t0])),xaxt="n");
+lines(d$V2[d$t > t0]~d$t[d$t > t0],col="blue")
+rug(s$V2[s$dt > 310 & s$V2 > t0],ticksize=0.2,col="red")
+rug(s$V2[s$V2 > t0],ticksize=0.1)
+axis.POSIXct(1, at=seq(as.POSIXct(t0,origin="1970-01-01",tz=tz),as.POSIXct(t1,origin="1970-01-01",tz=tz),3600),format="%H:%M")
+plot(g$V2~g$t,xlim=c(t0,t1),ylab="radiation (cpm)",xaxt="n",col="gray");
+lines(lowess(g$V2[g$t > t0]~g$t[g$t > t0],f=0.1),lwd=2,col="blue")
+axis.POSIXct(1, at=seq(as.POSIXct(t0,origin="1970-01-01",tz=tz),as.POSIXct(t1,origin="1970-01-01",tz=tz),3600),format="%H:%M")
+## dust sensor + voc TGS2602 in blue, coming from arduino
+plot(d$V3~d$t,type="l",xlim=c(t0,t1),ylim=c(0,2000),ylab="dust (mV)",xaxt="n",col="gray")
+lines(lowess(d$V3[d$t > t0]~d$t[d$t > t0],f=0.01),lwd=2,col="blue")
+axis.POSIXct(1, at=seq(as.POSIXct(t0,origin="1970-01-01",tz=tz),as.POSIXct(t1,origin="1970-01-01",tz=tz),3600),format="%H:%M")
+#axis.POSIXct(1, at=seq(as.POSIXct(t0,origin="1970-01-01",tz=tz),as.POSIXct(t1,origin="1970-01-01",tz=tz),3600),format="%H:%M")
+#format.POSIXct(cat(as.POSIXct(s$V2[length(s$V2)],origin="1970-01-01",tz=tz)),format="%c")
+plot(w$gas ~ w$time,type="l",xlim=c(t0,t1),ylim=c(min(w$gas[w$time > t0]),max(w$gas[w$time > t0])),ylab="VOCs (mV)",xaxt="n")
+axis.POSIXct(1, at=seq(t0,t1,by="hour"),format="%H:%M")
+circadian(t0,t1)
+plot(w$light ~ w$time,type="l",ylab="light (mV)",xlim=c(t0,t1))
+axis.POSIXct(1, at=seq(t0,t1,by="hour"),format="%H:%M")
+circadian(t0,t1)
+abline(v=w$time[w$tea > 0 & w$time > t0],col="green")
+lines(lowess(w$dust ~ w$time,f=0.2),lwd=2)
+plot(w$temperature ~ w$time,type="l",ylab="temperature (C)",xlim=c(t0,t1),ylim=c(min(w$temperature[w$time > t0]),max(w$temperature[w$time > t0])))
+axis.POSIXct(1, at=seq(t0,t1,by="hour"),format="%H:%M")
+circadian(t0,t1)
+plot(w$humidity ~ w$time,type="l",ylab="humidity (%Rh)",xlim=c(t0,t1),ylim=c(min(w$humidity[w$time > t0]),max(w$humidity[w$time > t0])))
+axis.POSIXct(1, at=seq(t0,t1,by="hour"),format="%H:%M")
+circadian(t0,t1)
